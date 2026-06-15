5dd8542195716e4d5270aa55cf905ca929f164bc
shipatulhasan <shipatulhasan328@gmail.com>
2026-06-15 20:18:13 +0600

    update list data and operation in python

---
+++ list-data-operation.py
@@ -0,0 +1,28 @@
+grocery_list =['egg','milk','bread']
+#in python push something using list
+grocery_list.append('apple')
+
+print (grocery_list) 
+#insert using the position
+grocery_list.insert(1,'banana')
+print (grocery_list) 
+
+# remove element
+# grocery_list.remove('banana')
+# print (grocery_list) 
+
+# using pop
+
+# grocery_list.pop(3)
+# print (grocery_list) 
+
+#loop
+if "egg" in grocery_list:
+  print('yes got the egg');
+# how to check the lenght in python len(list)
+print(len(grocery_list))
+
+# sorting list alphabaticaly list.sort()
+grocery_list.sort()
+print (grocery_list)
+
